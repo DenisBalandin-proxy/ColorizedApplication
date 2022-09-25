2afaf052d7421be96057ceeabc833defee9728bb
Denis Balandin <doublestar2013@yandex.ru>
2022-09-25 21:59:43 +0300

    FinishedApp

--- ColorizedApplication/ViewController.swift
+++ ColorizedApplication/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var rgbSliders: [UISlider]!
+    @IBOutlet var rgbLabels: [UILabel]!
+    @IBOutlet var colorizedView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureInitialSettings()
     }
-
-
+    
+    @IBAction func changeColors(_ sender: UISlider) {
+        switch sender.tag {
+        case 0:
+            rgbLabels[0].text = "Red: \(round(sender.value * 100) / 100.0)"
+        case 1:
+            rgbLabels[1].text = "Green: \(round(sender.value * 100) / 100.0)"
+        default:
+            rgbLabels[2].text = "Blue: \(round(sender.value * 100) / 100.0)"
+        }
+        blendColors()
+    }
+    
+    private func configureInitialSettings() {
+        colorizedView.layer.cornerRadius = 10
+        blendColors()
+    }
+    
+   private func blendColors() {
+        colorizedView.backgroundColor = UIColor(
+            red: CGFloat(rgbSliders[0].value),
+            green: CGFloat(rgbSliders[1].value),
+            blue: CGFloat(rgbSliders[2].value), alpha: 1
+        )
+    }
 }
-
